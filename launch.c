60b1586316c720d5d2aa9e7ce5d58bed73ecb8b0
elvis wangari <elviswangare@gmail.com>
2023-09-19 20:36:27 +0300

    betty

--- launch.c
+++ launch.c
@@ -1,31 +1,36 @@
 #include "main.h"
 /**
  * launch - a function to read the buffer
- * 
+ *
  * @args: args to be executed
  * @background: the processes id running in the background
- * 
- * Return: returns 1 
+ *
+ * Return: returns 1
 **/
-int launch(char *args[], int background) {
-    pid_t pid;
-    int status;
+int launch(char *args[], int background)
+{
+	pid_t pid;
+	int status;
 
-    pid = fork();
-    if (pid == 0) {
-        if (execvp(args[0], args) == -1) {
-            perror("shell");
-            exit(EXIT_FAILURE);
-        }
-    } else if (pid < 0) {
-        perror("shell");
-    } else {
-        if (!background) {
-            do {
-                pid = waitpid(pid, &status, WUNTRACED);
-            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
-    }
-
-    return (1);
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("shell");
+			exit(EXIT_FAILURE);
+		}
+	} else if (pid < 0)
+	{
+		perror("shell");
+	} else
+	{
+		if (!background)
+		{
+			do {
+				pid = waitpid(pid, &status, WUNTRACED);
+			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		}
+	}
+	return (1);
 }
--- split_line.c
+++ split_line.c
@@ -7,11 +7,11 @@
 **/
 int split_line(char *line, char *tokens[])
 {
-	int token_count = 0; 
+	int token_count = 0;
 	char *token = strtok(line, " \t\n");
 
 	while (token != NULL)
-	{ 
+	{
 		tokens[token_count] = token;
 		token_count++;
 		token = strtok(NULL, " \t\n");
