4e4b6ba767d993d137fb96c75b39812600eb4932
elvis wangari <elviswangare@gmail.com>
2023-10-09 23:30:29 +0300

    testing betty

--- main.h
+++ main.h
@@ -2,19 +2,5 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
 
-#define MAX_INPUT_SIZE 1024
-#define MAX_TOKENS 64 
-
-void read_line(char *buffer);
-int split_line(char *line, char *tokens[]);
-int launch(char *args[], int background); 
-int execute(char *args[]);
-
-#endif /* MAIN_H */
+#endif /*MAIN_H*/
---
+++ pass_betty.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - where the test function  starts
+ * Return: zero on success
+ */
+
+int main(void)
+{
+	printf("test if betty passes");
+
+	return (0);
+}
